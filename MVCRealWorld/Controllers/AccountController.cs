d92bd6fd626ff4ba3a265213d5f258442c6f636f
caofangsheng93 <fangsheng93@163.com>
2016-05-11 16:48:28 +0800

    登陆的方法

--- MVCRealWorld/Controllers/AccountController.cs
+++ MVCRealWorld/Controllers/AccountController.cs
@@ -38,5 +38,49 @@ namespace MVCRealWorld.Controllers
 
             return View();
         }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserLoginView model,string returnUrl)
+        {
+            /*
+             登陆逻辑，根据用户输入的用户名和密码，找出数据库中，对应用户的密码。
+             1.如果密码为空，则判断是用户名或者密码错误。
+             2.如果密码不为空，
+              * 2.1  再接着判断用户输入的密码和数据库中查到的密码是否相等。相等的话， 这个时候就设置Cookie，登陆的票据等，然后跳转到登陆之后的页面。
+              * 2.2   不相等的话，就判断密码错误。
+             */
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                //获取密码
+                string password = UM.GetUserPassword(model.UserName);
+                if (string.IsNullOrEmpty(password))
+                {
+                    ModelState.AddModelError("", "用户名或密码错误");
+
+                }
+                else
+                {
+                    if (model.UserPassword.Equals(password))
+                    {
+
+                        FormsAuthentication.SetAuthCookie(model.UserName, false);
+                        return RedirectToAction("Welcome", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "密码错误");
+                    }
+                }
+            }
+            //如果登陆失败的话，就停留在Login页面上
+            return View(model);
+        }
     }
 }
